976d1c33159acd46a619b730b92286a83c0d6cb0
dinamicaengenhariamkt <dinamicaengenhariamkt@gmail.com>
2026-02-22 03:11:23 +0000

    Página cliente e detalhes do mesmo reajustado conforme figma

--- .next/dev/server/chunks/ssr/_next-internal_server_app_dashboard_empreiteiras_page_actions_e6f6f1a5.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/dashboard/empreiteiras/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__) => {
-"use strict";
-
-__turbopack_context__.s([]);
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_dashboard_empreiteiras_page_actions_e6f6f1a5.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IgI3BohfoSl385kaJ04rttnib1seNlmnPuM8V4BD/bs=\"\n}"
